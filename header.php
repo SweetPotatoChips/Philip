9aec0d899a3dd2b0e874d3cad57028d712ccd1d0
Philip Bell <philhabell@gmail.com>
2016-03-10 17:17:41 +0000

    Added the makebooking,php and header.php file

---
+++ header.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <a class="navbar-brand"><b>Tour of Britian</b></a>
+        <ul class="nav navbar-nav">
+            <li><a href="index.html">Home</a></li>
+            <li class="active"><a href="makeBooking.html">Make Booking</a></li>
+            <li><a href="journeySearch.html">Journey Search</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="login.html">Login</a></li>
+            <li><a href="register.html">Register</a></li>
+        </ul>
+    </div>
+</nav>
---
+++ makeBooking.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>makeBooking</title>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="bootstrap-3.3.6-dist\css\bootstrap.min.css" >
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="bootstrap-3.3.6-dist\cssbootstrap-theme.min.css">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="bootstrap-3.3.6-dist\jsbootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid" style="padding-left:100px;padding-right:100px;padding-top:20px;">
+            <?php 
+            include "header.php"; 
+            ?>
+            <form name="starEndLoc">
+                <div class="input-group">
+                    <h4>Enter your start location:</h4>
+                    <input type="text" class="form-control" placeholder="Start Location" >
+                    <h4 style="padding-top:60px;">Enter your End location:</h4>
+                    <input tpye="text class" class="form-control" placeholder="End Location">
+                </div>
+            </form>
+    </body>
+</html> 
