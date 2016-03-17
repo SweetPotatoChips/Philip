bb2c89f909672e35db1d972aeb1bb595c91ea454
Philip Bell <philhabell@gmail.com>
2016-03-17 15:40:47 +0000

    attempted to change back to autoComplete

--- inputDest.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('#inputDest.typeahead').typeahead({
-		highlight:true
-		name: 'destinations',
-		local:[	"Warwick","Leamington Spa","Whitnash","Leamington Hastings","Coventry","Birminham","London","Liverpool"]
-	});
-});
-
--- makeBooking.php
+++ makeBooking.php
@@ -16,18 +16,32 @@
 			<br>
 			<h4>Please your desired start and end location below</h4>
 			<br>
+			<form action="submitBooking.php" method="get">
+				<script src="autoComplete.js"></script>
+				<div class="col-xs-3">
+					<input class="form-control" id="tags1" placeholder="Start Location" autocomplete="off">
+				</div>
+				<script src="autoComplete.js"></script>
+				<div class="col-xs-3">
+					<input class="form-control" id="tags2" placeholder="End Location" autocomplete="off">
+				</div>
+				<div class="col-xs-3">
+					<button class="btn btn-primary" type="submit">Submit</button>
+				</div>
+			</form>
+			<!--
 			<form action="makeBooking.php" method="get">
 				<div class="col-xs-3" id="inputDest">
 					<input class="typeahead" type="text" placeholder="Start Location" autocomplete="off">
 				</div>
-				<!--
+				
 				<div class="col-xs-3">
 					<input class="form-contol" id="tags" placeholder="End Location" autocomplete="off">
 				</div>
 				<div class="col-xs-3">
 					<button class="btn btn-primary" type="submit">Submit</button>
-				</div>-->
-			</form>
+				</div>
+			</form>-->
 	</body>
 </html> 
 
