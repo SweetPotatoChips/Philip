aeba1d331c74ced3f2752e45e3c1b9f2e56361bf
Philip Bell <philhabell@gmail.com>
2016-03-21 14:37:52 +0000

    started to add date

--- makeBooking.php
+++ makeBooking.php
@@ -35,6 +35,19 @@
 					<button class="btn btn-primary" type="submit">Submit</button><!-- submit button for the form -->
 				</div>
 
+				<script>
+					$('.datepicker').datepicker({
+    					format: 'mm/dd/yyyy',
+    					startDate: '-3d'
+						});
+				</script>
+				<div class="input-group date" data-provide="datepicker">
+				    <input type="text" class="form-control">
+				    <div class="input-group-addon">
+				        <span class="glyphicon glyphicon-th"></span>
+				    </div>
+				</div>
+
 			</form>
 
 			<br><br><br>
